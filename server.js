aebc4e9b6d7c637438abbf4df0f3ca2748bb73bf
Anthony <25408995+apb305@users.noreply.github.com>
2023-01-04 21:12:59 -0500

    first commit

--- server.js
+++ server.js
@@ -16,8 +16,6 @@ connectDB();
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
-app.use(express.static("public"));
-
 //Cors
 app.use(cors());
 
