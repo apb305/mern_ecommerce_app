d51ddea65cee71ce4cfc134d8e24230b22f53f59
Anthony <25408995+apb305@users.noreply.github.com>
2023-01-14 14:03:26 -0500

    updates

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ require("dotenv").config();
 connectDB();
 
 //Init Middleware
-app.use(express.json({ limit: "50mb" }));
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //Cors
