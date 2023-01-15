0cf73e287a98430617a3d6be530162c4688d5817
Anthony <25408995+apb305@users.noreply.github.com>
2023-01-15 13:46:34 -0500

    updates

--- routes/products.js
+++ routes/products.js
@@ -5,13 +5,14 @@ require("../models/Products");
 const Products = mongoose.model("products");
 // const { cloudinary } = require("../config/cloudinary");
 
-router.get("/", async (req, res) => {
+router.post("/", async (req, res) => {
   try {
     const products = await Products.find();
     if (products) {
       res.status(200).json(products);
     }
   } catch (error) {
+    res.status(400).json("An error has occured")
     console.log(error);
   }
 });
