f7773eb8d83de85434cf95698687424a6fb0953e
Anthony <25408995+apb305@users.noreply.github.com>
2023-03-08 23:00:55 -0500

    changes

--- client/src/pages/Reviews.js
+++ client/src/pages/Reviews.js
@@ -32,7 +32,7 @@ function Reviews() {
             <div>
               <div className="row d-flex justify-content-center">
                 <h1 className="text-center mt-2">Reviews</h1>
-                <small className="text-center">Reviews for {product.productName}</small>
+                {/* <small className="text-center">Reviews for {product.productName}</small> */}
                 <div className="p-4">
                   <div className="text-dark">
                     {/* Start of the review */}
