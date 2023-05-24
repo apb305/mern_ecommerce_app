06b3cc4163c8d2861b0fed8833a10d06262248c0
Anthony Bernard <25408995+apb305@users.noreply.github.com>
2023-05-24 13:44:27 -0400

    minor code updates

--- client/src/config/axiosConfig.js
+++ client/src/config/axiosConfig.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: process.env.HOST_URL
-})
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? process.env.HOST_URL
+      : "http://localhost:4000",
+});
 
-// const instance = axios.create({
-//     baseURL: "http://localhost:4000"
-// })
-
-export default instance; 
+export default instance;
